4a8a084d52940d7df9b647d00871b4f1c6f5f83e
Anders Hansen <xiplias@gmail.com>
2009-11-06 22:37:57 +0100

    Cleaned up old step definitions

--- features/step_definitions/itinerary_steps.rb
+++
@@ -1,4 +0,0 @@
-Then /^I should see the created trip page with the name "([^\"]*)"$/ do |arg1|
-  pending
-end
-
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,15 +1,3 @@
-Then /^I should be created as a new user$/ do
-  pending
-end
-
-Given /^I am a registered user$/ do
-  pending
-end
-
-When /^I fill in "([^\"]*)"  with "([^\"]*)"$/ do |arg1, arg2|
-  pending
-end
-
-Then /^I should be logged in$/ do
+Given /^I am logged in$/ do
   pending
 end
