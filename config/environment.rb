abb8e4823adbdec54167c9f41f77d1b06cfc257d
Anders Hansen <xiplias@gmail.com>
2009-11-06 17:38:51 +0100

    Added authlogic

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,9 @@ Rails::Initializer.run do |config|
   config.gem "nokogiri"
   config.gem "builder"
   
+  # User Authencation
+  config.gem "authlogic"
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
