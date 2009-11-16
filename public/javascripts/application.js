12af1c68aafdfbcf326bdfe44ad9e3690c9c9680
Anders Hansen <xiplias@gmail.com>
2009-11-16 23:24:29 +0100

    Added preminary autocomplete for add locations to map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var a;
+jQuery(function(){
+  a = $('#geocoding_search').autocomplete("/locations/find");
+
+  $("body").bind("ajaxSend", function(elm, xhr, s) {
+
+    // Set right accept
+    xhr.setRequestHeader("Accept", "text/javascript")
+
+    // Inserts auth_token if present
+    if (s.type == "GET") { return }
+    if (s.data && s.data.match(new RegExp("\\b" + window._auth_token_name + "="))) return;
+      if (s.data) {
+      s.data = s.data + "&";
+    } else {
+      s.data = "";
+      // if there was no data, $ didn't set the content-type
+      xhr.setRequestHeader("Content-Type", s.contentType);
+    }
+    s.data = s.data + encodeURIComponent(window._auth_token_name)+ "=" + encodeURIComponent(window._auth_token);
+  });
+});
+
