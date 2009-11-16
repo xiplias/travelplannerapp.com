2b344e17599d95276ab930eb92d9f0fbc9b88a9f
Anders Hansen <xiplias@gmail.com>
2009-11-16 23:23:53 +0100

    Added geokit to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,21 @@
 class Location < ActiveRecord::Base
+  acts_as_mappable :default_units => :kms, 
+                   :default_formula => :sphere, 
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
+  
   belongs_to :itinerary
   has_many :pois
+  
+  def self.find_location(query_name)
+    result = Geokit::Geocoders::GoogleGeocoder.geocode(query_name)
+    
+    locations = Array.new
+    
+    result.all.each do |r|
+      locations << [r.full_address, r.lat, r.lng]
+    end
+    
+    locations
+  end
 end
