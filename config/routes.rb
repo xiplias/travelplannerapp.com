c9f6789dd9b7dd1ddd3e089dd804d6cc715f755b
Anders Hansen <xiplias@gmail.com>
2009-11-16 23:25:48 +0100

    Added location find to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :itineraries
-
+  map.resources :itineraries, :has_many => :locations
+  map.resources :locations, :collection => {
+    :find => :get
+  }
 
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
