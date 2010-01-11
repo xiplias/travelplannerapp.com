fca010e7a1df95e0612b3eeb45a0799a89724233
Anders Hansen <xiplias@gmail.com>
2010-01-11 23:25:30 +0100

    Restricted interface only to work in itineraries

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,25 +20,6 @@ $(document).ready(function(){
     s.data = s.data + encodeURIComponent(window._auth_token_name)+ "=" + encodeURIComponent(window._auth_token);
   });
   
-  uki([
-      { view: 'Box', rect: '0 0 1000 31', anchors: 'left top right', background: 'theme(panel)',
-          childViews: { view: 'TextField', rect: '20 5 260 20', anchors: 'left', placeholder: 'Search', id: "lm_search" }
-      },
-      { view: 'SplitPane', rect: '0 30 1000 600', anchors: 'left top right bottom', 
-          handlePosition: 300, autogrowLeft: false, autogrowRight: true, autogrowLeft: false, handleWidth: 1, rightMin: 400, leftMin: 150,
-          leftChildViews:
-              { 
-                
-                view: 'ScrollPane', rect: '0 0 300 600', anchors: 'top left right bottom', background: '#d0d2d7', name: 'pane_1', id: "left_panel", scrollableH: true,
-              },
-          rightPane: {
-              childViews: { view: 'ScrollPane', rect: '0 0 699 600', anchors: 'top left right bottom',
-                  scrollableH: true, scrollableV: true, name: 'pane_2', id: "test"
-              }
-          }
-      }
-  ]).attachTo( window, '1000 630' );
-
   // uki('ScrollPane').attr('background', '#D0D7E2')
   // uki('ScrollPane').attr('background', '#0F0')
   // uki('ScrollPane')[0].dom().className = 'ScrollPane';
