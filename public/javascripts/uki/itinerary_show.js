eef0ec3ddd8a0c53c1f17f341fa7cad75cf26a81
Anders Hansen <xiplias@gmail.com>
2010-03-13 17:10:54 +0100

    Updated layout

--- public/javascripts/uki/itinerary_show.js
+++ public/javascripts/uki/itinerary_show.js
@@ -10,15 +10,32 @@ $(document).ready(function() {
 
 uki([
   { view: 'Box', rect: '0 0 1000 31', anchors: 'left top right', background: 'theme(panel)', childViews: [
-    { view: 'TextField', rect: '20 5 260 20', anchors: 'left', placeholder: 'Search', id: "lm_search" },
-    { view: 'Button', rect: '890 5 100 20', anchors: 'top right', id: "back_to_overview", visible: true, text: "Wide Mode"},
-    { view: 'Button', rect: '765 5 120 20', anchors: 'top right', id: "back_to_overview", visible: true, text: "Horisontal Mode"}
+    { view: 'Button', rect: '5 4 100 23', anchors: 'top left', id: "back_to_overview", visible: true, text: "Load Map"},
+    { view: 'Button', rect: '890 4 100 23', anchors: 'top right', id: "wide_mode", visible: true, text: "Wide Mode"},
+    { view: 'Button', rect: '765 4 120 23', anchors: 'top right', id: "horisontal_mode", visible: true, text: "Horisontal Mode"}
   ]},
   { view: 'SplitPane', rect: '0 31 1000 600', anchors: 'left top right bottom', id: 'second',
-      handlePosition: 279, autogrowRight: true, autogrowLeft: false, leftMin: 200, leftMax: 200, rightMin: 200, handleWidth: 1,
-      leftPane: { background: '#D0D7E2', id: 'left_panel', childViews: []},
+      handlePosition: 279, leftMin: 200, leftMax: 200, rightMin: 200, handleWidth: 1,
+      leftPane: { background: '#D0D7E2', childViews: [
+        { view: 'TextField', rect: '10 10 260 25', anchors: 'left top right', placeholder: 'Add Location', id: "lm_search" },
+        { view: 'ScrollPane', rect: '0 35 279 565', anchors: 'top left right bottom', id: 'left_panel' }
+      ]},
       rightChildViews: [
-          { view: 'VerticalSplitPane', rect: '720 600', anchors: 'left top right bottom', handlePosition: 320,
+        { view: 'ScrollPane', rect: '0 0 720 600', anchors: 'top left right bottom',
+             scrollableH: true, scrollableV: true, name: 'pane_2', id: "map"
+        }
+          /*{ view: 'SplitPane', rect: '720 600', anchors: 'left top right bottom', handlePosition: 593,
+              topChildViews: { view: 'ScrollPane', rect: '0 0 593 600', anchors: 'top left right bottom',
+                   scrollableH: true, scrollableV: true, name: 'pane_2', id: "map"
+              },
+              bottomPane: { background: '#FFF', childViews: [
+                  { view: 'Box', rect: '0 0 120 40', anchors: 'top right left bottom', background: 'cssBox(background:#EDF3FE;border-bottom:1px solid #999)' },
+                  { view: 'ScrollPane', rect: '0 0 120 600', anchors: 'top left right bottom',
+                       scrollableH: true, name: 'pane_2', id: "text"
+                  },
+              ]}
+          }
+          /*{ view: 'VerticalSplitPane', rect: '720 600', anchors: 'left top right bottom', handlePosition: 320,
               topChildViews: { view: 'ScrollPane', rect: '0 0 720 320', anchors: 'top left right bottom',
                    scrollableH: true, scrollableV: true, name: 'pane_2', id: "map"
               },
@@ -28,7 +45,11 @@ uki([
                        scrollableH: true, name: 'pane_2', id: "text"
                   },
               ]}
-          }
+          }*/
       ]
   }
 ]).attachTo( window, '1000 631', {minSize: '631 0'} );
+
+uki('#back_to_overview').click(function() {
+  alert("test");
+});
